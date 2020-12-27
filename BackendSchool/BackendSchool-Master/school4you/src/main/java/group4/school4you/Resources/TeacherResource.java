0dc37b09e91a51f8cfe3fb737f2014dbd83a486c
Andi <bart02@ads.uni-passau.de>
2020-12-27 15:05:07 +0100

    Teacher gets an overview of all classes he teaches

--- BackendSchool/BackendSchool-Master/school4you/src/main/java/group4/school4you/Resources/TeacherResource.java
+++ BackendSchool/BackendSchool-Master/school4you/src/main/java/group4/school4you/Resources/TeacherResource.java
@@ -1,23 +1,47 @@
 package group4.school4you.Resources;
 
-import group4.school4you.Entities.Student;
+
 import group4.school4you.Entities.Teacher;
-import group4.school4you.Repositories.StudentRepository;
+import group4.school4you.Entities.schoolClass;
+import group4.school4you.Repositories.AnnouncementRepository;
 import group4.school4you.Repositories.TeacherRepository;
+import group4.school4you.Repositories.schoolClassRepository;
 import group4.school4you.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class TeacherResource {
     @Autowired
     private TeacherRepository teacherRepository;
-
+    private AnnouncementRepository announcementRepository;
+    private schoolClassRepository schoolClassRepository;
     private UserService userService;
 
+    /**
+     * This methods gets an id from the frontend of a certain teacher and searches in the database for all classes
+     * which contain this teacher in its list of teachers. Then it collects all this classes to give a list of all
+     * classes teached by this teacher back to the frontend.
+     * @param id To get the teacher from the repository.
+     * @return A list of all classes which the teachers teaches.
+     */
+    @GetMapping(path = "/teacher/classes/{id}")
+    public List<schoolClass> getClasses(@PathVariable long id){
+        List<schoolClass> allClasses = schoolClassRepository.findAll();
+        List<schoolClass> toReturn = null;
+        Teacher toGetClasses = (Teacher) teacherRepository.findById(id).get();
 
+        for(int i = 0; i < allClasses.size(); i++){
+            if(allClasses.get(i).containsTeacher(toGetClasses)) {
+                toReturn.add(allClasses.get(i));
+            }
+        }
+        return toReturn;
+    }
 }
